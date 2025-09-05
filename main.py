cfb8ac3c595859ba3fa198001656eaab8958ee77
Shifana Sharafudeen <146415302+ShifArshad@users.noreply.github.com>
2025-09-05 12:21:09 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+import requests
+from bs4 import BeautifulSoup
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+# Declaring needed Variables
+Spotifyusername = ""
+year = input("What year would you like to travel to in YY-MM-DD? ")
+URL = f"https://www.billboard.com/charts/hot-100/{year}"
+CLIENT_ID = ""
+CLIENT_SECRET = ""
+Redirect_URL = "https://example.com/callback"
+scope = "playlist-read-private, " \
+        "user-read-currently-playing, " \
+        "user-read-currently-playing, " \
+        "user-follow-read, playlist-modify-private"
+
+
+# SCRAPING BILLBOARD TOP 100 CHARTS WITH BEAUTIFUL SOUP
+response = requests.get(URL)
+billboard_website = response.text
+soup = BeautifulSoup(billboard_website, "html.parser")
+top100 = soup.find_all("h3", class_="u-max-width-330")
+
+# Authenticating Spotify client
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    redirect_uri=Redirect_URL,
+    scope=scope)
+)
+
+song_uris = []
+for song in top100:
+    song_name = song.getText().strip()
+    result = sp.search(q=song_name, type="track")
+
+    if result["tracks"]["items"]:
+        song_uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(song_uri)
+    else:
+        print(f"Song {song_name} not found on Spotify")
+
+# Creating a new playlist on Spotify
+playlist_name = f"Billboard Hot 100 - {year}"
+description = "Top 100 songs on Billboard charts for the specified year."
+
+playlist = sp.user_playlist_create(user=Spotifyusername,
+                                   name=playlist_name,
+                                   description=description,
+                                   public=False)
+# Adding songs to playlist
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
+
+user_id = sp.current_user()["id"]
